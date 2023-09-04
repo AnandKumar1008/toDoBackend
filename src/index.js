7bbe5edaf802c3e6450c8c6ec0b25ad3ac66ce68
AnandKumar <anandkumar031199@gmail.com>
2023-09-04 16:56:14 +0530

    update changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 1008;
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(url, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -17,4 +17,4 @@ async function connectToMongoDB() {
 }
 
 connectToMongoDB();
-app.listen(port, () => console.log("Server is up and running"));
+app.listen(port, () => console.log("Server is up and running", port));
